3071559e09c6760f70729b625a79d6e6f82250ea
ad3kva1 <tukann.tukann@mail.ru>
2025-04-25 15:28:29 +0300

    ...

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,12 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
         gradlePluginPortal()
+        google()
+        mavenCentral()
     }
 }
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
@@ -21,4 +15,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "CIAP"
 include(":app")
- 
